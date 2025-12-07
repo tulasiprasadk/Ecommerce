646560a907d2d8ce9f10da49d5b3bff012d4af7f
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-08 04:32:26 +0530

    Fix prisma import path to use '@/generated/prisma'

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "../../src/generated/prisma";
+import { PrismaClient } from "@/generated/prisma";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import Database from "better-sqlite3";
 
+// Vercel SQLite DB file (inside ephemeral FS)
 const db = new Database("/vercel/path0/sqlite.db");
 
 let prisma: PrismaClient;
@@ -11,6 +12,7 @@ if (process.env.NODE_ENV === "production") {
     adapter: new PrismaBetterSqlite3(db),
   });
 } else {
+  // Prevent creating multiple instances in dev
   if (!(global as any).prisma) {
     (global as any).prisma = new PrismaClient({
       adapter: new PrismaBetterSqlite3(db),
